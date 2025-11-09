82ff7781061d8d1dcfb1aedda38806e7618d36dd
Jonathan Lagneaux <lagneaux.j@gmail.com>
2025-11-09 17:12:04 +0100

    feat: add new price indicator component

---
+++ composeApp/src/commonMain/kotlin/components/PriceIndicator.kt
@@ -0,0 +1,62 @@
+package components
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import kotlinx.coroutines.delay
+import kotlin.math.pow
+import kotlin.math.roundToInt
+import kotlin.random.Random
+
+
+@Composable
+fun PriceIndicator(price: Double) {
+    var priceState by remember { mutableStateOf(price) }
+
+    val oldPrice = remember(price) {
+        val prev = priceState
+        priceState = price // Update for the *next* recomposition
+        prev
+    }
+    val indicatorColor by animateColorAsState(
+        targetValue = when {
+            price > oldPrice -> Color.Green
+            price < oldPrice -> Color.Red
+            else -> Color.Gray
+        },
+        animationSpec = tween(durationMillis = 500),
+        label = "priceIndicatorColor"
+    )
+
+    val formattedPrice = price.formatDecimal(2)
+    Text(text = "Price: $formattedPrice €", color = indicatorColor)
+}
+
+@Composable
+fun RealtimePriceIndicator() { // only exist for demo purposes
+    var randomPrice by remember { mutableStateOf(Random.nextDouble() * 100) }
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            delay(2000) // Wait for 2 seconds
+            randomPrice = Random.nextDouble() * 100 // Update the state with a new random value
+        }
+    }
+
+    Column {
+        PriceIndicator(randomPrice)
+    }
+}
+
+private fun Double.formatDecimal(digits: Int): String {
+    val factor = 10.0.pow(digits)
+    return ((this * factor).roundToInt() / factor).toString()
+}
--- composeApp/src/commonMain/kotlin/pages/Components.kt
+++ composeApp/src/commonMain/kotlin/pages/Components.kt
@@ -3,10 +3,10 @@ package pages
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.FilledTonalButton
@@ -19,6 +19,7 @@ import components.CircularButton
 import components.ConditionalWebView
 import components.FilterChipExample
 import components.RangeSlider
+import components.RealtimePriceIndicator
 import components.SliderMinimal
 import components.SwitchMinimal
 import components.SwitchWithIcon
@@ -30,56 +31,72 @@ import org.jetbrains.compose.resources.painterResource
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun Components() {
-    FlowRow(
-        modifier = Modifier.verticalScroll(rememberScrollState())
-            .padding(16.dp),
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 300.dp), // This creates the responsive multi-column layout
         horizontalArrangement = Arrangement.spacedBy(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(16.dp),
     ) {
-        ComponentsBlock(
-            title = "Buttons",
-            description = "In the realm of user interfaces, common buttons wield their magical influence, conjuring forth the majority of actions with a mere touch – a seamless spellbinding interaction for every user."
-        ) {
-            Button(onClick = {}) {
-                Text("Button default")
-            }
-            ElevatedButton(onClick = {}) {
-                Text("Button elevated")
-            }
-            FilledTonalButton(onClick = {}) {
-                Text("Button tonal")
+        item {
+            ComponentsBlock(
+                title = "Buttons",
+                description = "In the realm of user interfaces, common buttons wield their magical influence, conjuring forth the majority of actions with a mere touch – a seamless spellbinding interaction for every user."
+            ) {
+                Button(onClick = {}) { Text("Button default") }
+                ElevatedButton(onClick = {}) { Text("Button elevated") }
+                FilledTonalButton(onClick = {}) { Text("Button tonal") }
             }
         }
-        ComponentsBlock(
-            title = "Icon buttons",
-            description = "Behold the mystical prowess of icon buttons, granting individuals the power to enact minor wonders with a mere touch – a spellbinding experience at the tip of your fingers."
-        ) {
-            CircularButton(painterProvider = { painterResource(Res.drawable.magic) }) { }
-            CircularButton(painterProvider = { painterResource(Res.drawable.plus) }) { }
-        }
-        ComponentsBlock(
-            title = "Chips",
-            description = "Enchanting with its prowess, Magic Chips elegantly guides individuals to effortlessly weave through realms of information, craft selections, refine content, and conjure actions at their fingertips."
-        ) {
-            FilterChipExample()
-            AssistChip()
-        }
-        ComponentsBlock(
-            title = "Switch",
-            description = "Enchantments manifest as switches, weaving the arcane dance to toggle the mystical selection of an item, granting the power to illuminate or shroud in the whims of magic."
-        ) {
-            SwitchMinimal()
-            SwitchWithIcon()
-        }
-        ComponentsBlock(
-            title = "Sliders",
-            description = "Sliders, the enchanted navigators of choice, beckon users to traverse the mystical spectrum of values, granting them the power to shape their selections with a mere glide – a magical journey through the realms of choice."
-        ) {
-            Column {
-                SliderMinimal()
-                RangeSlider()
+        item {
+            ComponentsBlock(
+                title = "Icon buttons",
+                description = "Behold the mystical prowess of icon buttons, granting individuals the power to enact minor wonders with a mere touch – a spellbinding experience at the tip of your fingers."
+            ) {
+                CircularButton(painterProvider = { painterResource(Res.drawable.magic) }) { }
+                CircularButton(painterProvider = { painterResource(Res.drawable.plus) }) { }
             }
         }
-        ConditionalWebView()
+        item {
+            ComponentsBlock(
+                title = "Chips",
+                description = "Enchanting with its prowess, Magic Chips elegantly guides individuals to effortlessly weave through realms of information, craft selections, refine content, and conjure actions at their fingertips."
+            ) {
+                FilterChipExample()
+                AssistChip()
+            }
+        }
+        item {
+            ComponentsBlock(
+                title = "Switch",
+                description = "Enchantments manifest as switches, weaving the arcane dance to toggle the mystical selection of an item, granting the power to illuminate or shroud in the whims of magic."
+            ) {
+                SwitchMinimal()
+                SwitchWithIcon()
+            }
+        }
+        item {
+            ComponentsBlock(
+                title = "Sliders",
+                description = "Sliders, the enchanted navigators of choice, beckon users to traverse the mystical spectrum of values, granting them the power to shape their selections with a mere glide – a magical journey through the realms of choice."
+            ) {
+                Column {
+                    SliderMinimal()
+                    RangeSlider()
+                }
+            }
+        }
+        item {
+            // Because it's in a lazy grid, this WebView will now be properly disposed of when scrolled off-screen.
+            ConditionalWebView()
+        }
+        item {
+            ComponentsBlock(
+                title = "Price Indicator",
+                description = "Behold the Oracle of Value, a mystical indicator that channels the ever-shifting tides of fortune. With a flicker of emerald green for rising prosperity or a flush of crimson for waning wealth, it grants seers a real-time glimpse into the unpredictable dance of the market's magic."
+            ) {
+                // Your price indicator will also benefit from this virtualization.
+                RealtimePriceIndicator()
+            }
+        }
     }
 }
